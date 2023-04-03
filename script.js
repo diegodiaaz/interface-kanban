1b309ee4d2230ea7f02ab9ff7891318630648fb7
Diego Henrique <diego.reis.ruiz@gmail.com>
2023-04-02 22:52:35 -0300

    html e css prontos

--- script.js
+++
@@ -1,8 +0,0 @@
-const elements = document.querySelectorAll('.kanban p')
-const LIMIT = 41
-
-for (let p of elements) {
-  const aboveLimit = p.innerText.length > LIMIT
-  const dotsOrEmpty = aboveLimit ? '...' : ''
-  p.innerText = p.innerText.substring(0, LIMIT) + dotsOrEmpty
-}
